b1ee2afae4060f1ed7944ac482790e95afb6f1e5
Lolkek4ik <158052830+Lolkek4ik@users.noreply.github.com>
2024-01-29 14:59:50 +0200

    Add files via upload

---
+++ Ldl1/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using Ldl1;
+
+internal class Program
+{
+    static Library library = new Library();
+
+    static void Main(string[] args)
+    {
+        byte abc = 1;
+        string nyes;
+        string nno;
+
+    exe:
+        Console.WriteLine("What would you like to execute?");
+        while (abc != 0)
+        {
+            Console.WriteLine("1) Add a book?");
+            Console.WriteLine("2) Remove a book?");
+            Console.WriteLine("3) Display all books in the library?");
+            Console.WriteLine("4) End");
+            try{
+                nno = Console.ReadLine();
+                
+                switch (nno)
+                {
+                    case "1":library.AddBook(); break;
+                    case "2":library.Remove(); break;
+                    case "3":library.Display(); break;
+                    case "4":abc = 0; break;
+                } 
+                
+            } 
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number."); goto exe;
+            }        
+
+        nyes:
+            Console.WriteLine("Would you like to continue? (yes/no)");
+            nyes = Console.ReadLine();
+            if (nyes == "no") { abc = 0; }
+            else if (nyes == "yes") { abc = 1; goto exe; }
+            else { Console.WriteLine("Write again"); goto nyes; }
+        }
+    }
+}
---
+++ Ldl1/library.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Ldl1
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public string ISBN;
+
+        public Book(string title, string author, string iSBN)
+        {
+            Title = title;
+            Author = author;
+            ISBN = iSBN;
+        }
+    }
+
+    internal class Library
+    {
+        private List<Book> books;
+
+        public Library()
+        {
+            books = new List<Book>();
+        }
+        //======================ADD==============================ADD===============================ADD=============================
+        public void AddBook()
+        {
+            Console.WriteLine("Enter the book's Title:");
+            string title = Console.ReadLine();
+            Console.WriteLine("Enter the book's Author:");
+            string author = Console.ReadLine();
+            Console.WriteLine("Enter the book's ISBN:");
+            string isbn = Console.ReadLine();
+
+            Book book = new Book(title, author, isbn);
+            books.Add(book);
+
+            Console.WriteLine($"Book '{book.Title}' added to the library.");
+        }
+
+        //=======================REMOVE==================REMOVE=======================REMOVE=======================================
+        public void Remove()
+        {
+            Console.WriteLine("Enter the ISBN of the book to remove:");
+            string isbn = Console.ReadLine();
+
+            int indexToRemove = -1;
+
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].ISBN == isbn)
+                {indexToRemove = i; break;}
+            }
+
+            if (indexToRemove != -1)
+            {
+                Book removedBook = books[indexToRemove];
+                books.RemoveAt(indexToRemove);
+                Console.WriteLine($"Book '{removedBook.Title}' removed from the library.");
+            }
+            else
+            {Console.WriteLine($"Book with ISBN {isbn} not found in the library.");}
+        }
+
+        //================DISPLAY=============================DISPLAY==========================DISPLAY===========================
+        public void Display()
+        {
+            Console.WriteLine("Books in the library:");
+            foreach (Book book in books)
+            {
+                Console.WriteLine($"{book.Title} ({book.Author})");
+            }
+        }
+    }
+}
